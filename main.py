99cf799a376bd922af0bbaedc8be95a7f14ec0b4
Richenel2 <yvanprogramme@gmail.com>
2025-09-10 15:25:46 +0100

    update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from utils import normalizeStr, normalizeUrl, strictMatch, similarlyMatch
-
+import json
 
 couplageFort = []
 couplageProbable = []
--- utils.py
+++ utils.py
@@ -27,13 +27,13 @@ def strictMatch(df : pd.DataFrame, column : str, score_explicatif : str) -> list
 
     return clusters_forts.to_dict(orient="records")
 
-def similarlyMatch(df : pd.DataFrame, column : str) -> list:
+def similarlyMatch(df : pd.DataFrame, column : str, score_explicatif : str) -> list:
     vectorizer = TfidfVectorizer(stop_words=list(STOP_WORDS))
     X = vectorizer.fit_transform(df[column])
     similarity_matrix = cosine_similarity(X)
     clustering = AgglomerativeClustering(
         n_clusters=None,             # pas de nombre fixe de clusters
-        affinity='precomputed',      # distance calculée
+        metric='precomputed',      # distance calculée
         linkage='complete',          # lien complet
         distance_threshold=0.15      # 1 - similarité ≈ distance
     )
@@ -41,5 +41,5 @@ def similarlyMatch(df : pd.DataFrame, column : str) -> list:
     clustering_labels = clustering.fit_predict(1 - similarity_matrix)
     df['cluster_proximity'] = clustering_labels
     print(clustering_labels)
-    return strictMatch(df, "cluster_proximity", "")
+    return strictMatch(df, "cluster_proximity", score_explicatif)
 
